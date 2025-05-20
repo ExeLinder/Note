f412a56855392768e096184493d2a9f62e548e4e
YarKrav <jaroslav.krava@gmail.com>
2025-05-20 14:12:00 +0300

    added ShowFiles method

--- Note/Note.cs
+++ Note/Note.cs
@@ -12,5 +12,33 @@ namespace Note
         private void WriteFile(string path) { }
         private void ShowFiles() { }
         public void Menu() { }
+        private void ShowFiles(string folderPath)
+        {
+            folderPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Notes");
+
+            Console.WriteLine("Your folder: " + folderPath + "\n");
+
+            if (!Directory.Exists(folderPath))
+            {
+                Console.WriteLine("Notes folder not found. Creating...");
+                Directory.CreateDirectory(folderPath);
+                return;
+            }
+
+            string[] files = Directory.GetFiles(folderPath);
+
+            if (files.Length == 0)
+            {
+                Console.WriteLine("There are no notes.");
+                return;
+            }
+
+            Console.WriteLine("List of notes:");
+            foreach (var file in files)
+            {
+                Console.WriteLine("- " + Path.GetFileName(file));
+            }
+        }
+
     }
 }
