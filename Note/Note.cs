9a43c29f0b41ae048f0eb67a6c72bedd476baaf0
Sergey Ivanov <mailto.sergeyivanov@gmail.com>
2025-05-20 12:28:53 +0300

    Add project files.

---
+++ Note/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Note
+{
+    internal class Note
+    {
+        private void PrintFile(string path) { }
+        private void WriteFile(string path) { }
+        private void ShowFiles() { }
+        public void Menu() { }
+    }
+}
